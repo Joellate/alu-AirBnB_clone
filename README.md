2bd7049b9e54ab5a84532461c184e4a4648c01d9
Joellate <j.teta@alustudent.com>
2025-05-13 23:21:58 -0700

    Update console and README; add test files for save/reload

--- README.md
+++ README.md
@@ -1,18 +1,1 @@
-# ALU AirBnB Clone
-
-This project is a clone of the AirBnB web application, developed as part of the ALX Software Engineering program. It is the first step towards building a full web application — starting with a command-line interface (CLI) that simulates the backend and data storage operations.
-
-## Description
-
-The goal of this project is to build a simplified version of the AirBnB platform using Python. It includes a custom command interpreter to manipulate data stored in a JSON file, emulating a database. The application manages different objects such as users, places, reviews, etc., through object-oriented programming.
-
-## Command Interpreter
-
-The command interpreter provides an interactive shell environment to manage the AirBnB data models. It allows users to create, update, delete, and display object instances from the command line.
-
-### How to Start It
-
-1. Clone the repository:
-   ```bash
-   git clone https://github.com/YOUR_USERNAME/alu-AirBnB_clone.git
-cd alu-AirBnB_clone
+alu-AirBnb
--- console.py
+++ console.py
@@ -1,50 +1,111 @@
 #!/usr/bin/python3
+"""console"""
 import cmd
 from models.base_model import BaseModel
 from models import storage
+from models import classes
+
 
 class HBNBCommand(cmd.Cmd):
     prompt = '(hbnb) '
 
-    def do_create(self, line):
-        if not line:
-            print("** class name missing **")
-            return
-        if line == "BaseModel":
-            obj = BaseModel()
-            obj.save()
-            print(obj.id)
-        else:
-            print("** class doesn't exist **")
-
-    def do_show(self, line):
-        parts = line.split()
-        if len(parts) == 0:
-            print("** class name missing **")
-        elif parts[0] != "BaseModel":
-            print("** class doesn't exist **")
-        elif len(parts) < 2:
-            print("** instance id missing **")
-        else:
-            key = f"{parts[0]}.{parts[1]}"
-            all_objs = storage.all()
-            if key in all_objs:
-                print(all_objs[key])
-            else:
-                print("** no instance found **")
-
-    def do_quit(self, line):
+    def do_quit(self, arg):
         """Quit command to exit the program"""
         return True
 
-    def do_EOF(self, line):
-        """EOF (Ctrl+D) to exit the program"""
-        print()
+    def do_EOF(self, arg):
+        """Exit the program on EOF"""
         return True
 
     def emptyline(self):
+        """nothing done when an empty line is entered"""
         pass
+
+    def do_create(self, arg):
+
+        if not arg:
+            print("** class name missing **")
+        elif arg not in classes:
+            print("** class doesn't exist **")
+        else:
+            model = classes[arg]()
+            storage.new(model)
+            storage.save()
+            print(model.id)
+
+    def help_create(self):
+        print("Create command to create a new object")
+
+    def do_show(self, arg):
+        if not arg:
+            print("** class name missing **")
+        else:
+            arr = arg.split(" ")
+            if arr[0] not in classes.keys():
+                print("** class doesn't exist **")
+            elif len(arr) < 2:
+                print("** instance id missing **")
+            else:
+                all_items = storage.all()
+                item = all_items.get("{}.{}".format(arr[0], arr[1]))
+                if not item:
+                    print("** no instance found **")
+                else:
+                    print(item)
+
+    def do_destroy(self, arg):
+        if not arg:
+            print("** class name missing **")
+        else:
+            arr = arg.split(" ")
+            if arr[0] not in classes.keys():
+                print("** class doesn't exist **")
+            elif len(arr) < 2:
+                print("** instance id missing **")
+            else:
+                all_items = storage.all()
+                item = all_items.get("{}.{}".format(arr[0], arr[1]))
+                if not item:
+                    print("** no instance found **")
+                else:
+                    del all_items["{}.{}".format(arr[0], arr[1])]
+                    storage.save()
+
+    def do_all(self, arg):
+        if not arg:
+            items = storage.all()
+            arr = [str(items[i]) for i in items.keys()]
+            print(arr)
+        else:
+            if arg not in classes.keys():
+                print("** class doesn't exist **")
+            else:
+                items = storage.all()
+                arr = [str(items[i]) for i in items.keys()
+                       if items[i].__class__.__name__ == arg]
+                print(arr)
+
+    def do_update(self, arg):
+        arr = arg.split(" ")
+        if not arg:
+            print("** class name missing **")
+        elif arr[0] not in classes.keys():
+            print("** class doesn't exist **")
+        elif len(arr) < 2:
+            print("** instance id missing **")
+        else:
+            items = storage.all()
+            item = items.get("{}.{}".format(arr[0], arr[1]))
+            if not item:
+                print("** no instance found **")
+            elif len(arr) < 3:
+                print("** attribute name missing **")
+            elif len(arr) < 4:
+                print("** value missing **")
+            else:
+                setattr(item, arr[2], arr[3])
+                storage.save()
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
-
